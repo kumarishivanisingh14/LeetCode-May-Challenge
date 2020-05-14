0f2bf9e1eb57b54e2610f662744910a63492beff
kumarishivanisingh14 <43853444+kumarishivanisingh14@users.noreply.github.com>
2020-05-14 19:36:21 +0530

    Update 7. Implement Trie (Prefix Tree).py

--- Week-2/7. Implement Trie (Prefix Tree).py
+++ Week-2/7. Implement Trie (Prefix Tree).py
@@ -1,3 +1,22 @@
+Implement a trie with insert, search, and startsWith methods.
+
+Example:
+Trie trie = new Trie();
+
+trie.insert("apple");
+trie.search("apple");   // returns true
+trie.search("app");     // returns false
+trie.startsWith("app"); // returns true
+trie.insert("app");   
+trie.search("app");     // returns true
+
+
+Note:
+1. You may assume that all inputs are consist of lowercase letters a-z.
+2. All inputs are guaranteed to be non-empty strings.
+
+
+Solution:
 class  TrieNode:
     def __init__(self,char):
         self.char = char
